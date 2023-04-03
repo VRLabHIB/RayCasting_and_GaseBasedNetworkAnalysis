5b5d3f09517e19ceb3b8fe7583e9d07fc1511b8f
Philipp Stark <74495398+VRLabHIB@users.noreply.github.com>
2023-04-03 16:39:09 +0200

    update paper

--- GazeBasedNetworkAnalysis/fl3_calculate_graph_features.py
+++ GazeBasedNetworkAnalysis/fl3_calculate_graph_features.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 import networkx as nx
 
+from scipy.stats import chisquare
+
 ### Helper functions
 def split(word):
     return [char for char in word]
@@ -54,11 +56,51 @@ def degree_centrality_for_groups(graph_lst, name_lst, group_name, names_of_group
     return df
 
 
-def weighted_degree_centrality_for_uniformity():
-    return None
+def weighted_degree_centrality_for_uniformity(graph_lst, name_lst, node):
+    ID_lst = list()
+    WDC_lst = list()
+
+    for graph in range(len(graph_lst)):
+        G = graph_lst[graph]
+        s = split(name_lst[graph])
+        ID = s[0] + s[1] + s[2]
+
+        # Weighted degree centrality for specific node
+        edges = G.out_edges(node, data=True)
+        sumw = G.out_degree(node, weight='Weight')
+
+        weight_lst = list()
+
+        for e in edges:
+            source, target, weight = e
+            weight_lst.append(weight['Weight'])
+
+        # this if case is not in the paper, sorting is important
+        weight_lst.sort()
+
+        DC = len(edges)
+        AUCmax = DC / 2
+        FCi = np.zeros(DC - 1)
+
+        for i in range(DC - 1):  # 0,1,2,3
+            fj = 0
+            for j in range(i + 1):
+                fj += weight_lst[j] / sumw
+            FCi[i] = fj
+
+        AUCFc = (1 / 2) + (np.sum(FCi))
+
+        R = AUCFc / AUCmax
+
+        WDC = R * DC
+
+        WDC_lst.append(WDC)
+    df = pd.DataFrame({'ID':ID_lst, 'WDC':WDC_lst})
+    return df
 
 
-def clique_features(graph_lst, name_lst, group_name, names_of_group_members, number_of_cliques=True, avg_clique_size=True):
+def clique_features(graph_lst, name_lst, group_name, names_of_group_members,
+                    number_of_cliques=True, avg_clique_size=True):
     for graph in range(len(graph_lst)):
         G = graph_lst[graph]
         name = name_lst[graph]
@@ -116,16 +158,56 @@ def clique_features(graph_lst, name_lst, group_name, names_of_group_members, num
         avg_clique_size_stud_lst.append(n_nodes_in_c_stud)
         n_cl_lst_not.append(n_cl_not)
         if number_of_cliques:
+            None
 
     return None
 
 
-def cut_size():
+def cut_size(graph_lst, name_lst):
+    cut_norm_lst = list()
+    ID_lst = list()
+
+    for graph in range(len(graph_lst)):
+        G = graph_lst[graph]
+        s = split(data_lst[graph])
+        ID = s[0] + s[1] + s[2]
+
+        # Devide nodes into students and teacher/screen
+        S = list()
+        for i in G.nodes:
+            if i in stud_lst:
+                S.append(i)
+        T = ['teacher', 'screen']
+
+        # calculate cut size between the two groups
+        cut_norm = nx.normalized_cut_size(G, S, T, weight='Weight')
+
+        ID_lst.append(ID)
+        cut_norm_lst.append(cut_norm)
+
     return None
 
 
-def uniformity():
-    return None
+def uniformity(graph_lst, name_lst):
+    ID_lst = list()
+    chi_lst = list()
+
+    for graph in range(len(graph_lst)):
+        G = graph_lst[graph]
+        s = split(name_lst[graph])
+        ID = s[0] + s[1] + s[2]
+
+        n_nodes = len(list(G.nodes))
+        x = nx.get_edge_attributes(G, 'Weight')
+        x = list(x.values())
+
+        chi, _ = chisquare(x)
+
+        ID_lst.append(ID)
+        chi_lst.append(-chi)
+
+    df = pd.DataFrame({'ID': ID_lst, 'ChiUniformity':chi_lst})
+    return df
 
 if __name__ == '__main__':
     #Load a graph and test
