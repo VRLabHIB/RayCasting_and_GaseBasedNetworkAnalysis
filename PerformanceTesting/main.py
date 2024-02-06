863855a057d070670a690e55fae61adcbe10f01e
VRESoftware <102028759+VRESoftware@users.noreply.github.com>
2024-02-06 22:17:54 +0100

    run testing

--- PerformanceTesting/main.py
+++ PerformanceTesting/main.py
@@ -26,16 +26,16 @@ def create_OOI_letters():
 def generate_dataset(project_path, dataset_size, fps, n_OOI):
     # Create time dataframe
     list_cols = ['time']
-    df = pd.DataFrame(np.arange(0, dataset_size * fps, 0.02), columns=list_cols)
+    df = pd.DataFrame(np.arange(0, dataset_size * fps, 0.025), columns=list_cols)
 
     # Create OOI variable (for comparison reason choose OOIs to switch every timeframe)
     letters = create_OOI_letters()
     letters_n_OOI = letters[:n_OOI]
     lst = list()
     for l in letters_n_OOI:
-        l10 = [l] * 10
+        l10 = [l] * 8
         lst = lst + l10
-    lst = lst * int(dataset_size / 10)
+    lst = lst * int(dataset_size / 8)
     #OOI_lst = lst[:n_OOI]
     OOI_var = lst[:dataset_size]# * int(dataset_size / len(OOI_lst))
 
@@ -55,7 +55,7 @@ def load_dataframes(project_path, Npart, dataset_size, N_OOI):
     dataframes = list()
 
     for i in range(Npart):
-        df = pd.read_csv(data_path + 'df_size-{}_fps-002_nOOIS-{}.csv'.format(dataset_size, N_OOI), index_col=0)
+        df = pd.read_csv(data_path + 'df_size-{}_fps-0025_nOOIS-{}.csv'.format(dataset_size, N_OOI), index_col=0)
         dataframes.append(df)
 
     dataloader_deltatime = time.time() - dataloader_time
@@ -72,21 +72,23 @@ if __name__ == '__main__':
     project_path = os.path.abspath(os.getcwd())
 
     # Performance tracking
-    dataloader_file = open(project_path + "/results/Python/dataloader.txt", "w")
-    dataloader_file.write("Participants, Dataset_size, NumberOfOOIS, runtime, memorycurrent, memorypeak \n")
+    #dataloader_file = open(project_path + "/results/Python/dataloader.txt", "w")
+    #dataloader_file.write("Participants, Dataset_size, NumberOfOOIS, runtime, memorycurrent, memorypeak \n")
+    #dataloader_file.close()
 
-    matrices_file = open(project_path + "/results/Python/matrices.txt", "w")
-    matrices_file.write("Participants, Dataset_size, NumberOfOOIS, runtime, memorycurrent, memorypeak \n")
+    #matrices_file = open(project_path + "/results/Python/matrices.txt", "w")
+    #matrices_file.write("Participants, Dataset_size, NumberOfOOIS, runtime, memorycurrent, memorypeak \n")
+    #matrices_file.close()
 
     # Enable steps in testing
     generate_data = False
     build_transition_matrices = True
 
     # Parameters
-    N_participants = [25, 50, 100, 250, 500]
-    dataset_sizes = [100, 1000, 10000, 100000]
-    N_OOIs = [4, 10, 25, 50]
-    fps = 0.02
+    N_participants = [50, 100, 300] # done
+    dataset_sizes = [25000, 50000]
+    N_OOIs = [10, 20, 40]
+    fps = 0.025
 
     # Generate test datasets
     if generate_data:
@@ -98,41 +100,44 @@ if __name__ == '__main__':
 
     # Build transition matrices
     if build_transition_matrices:
-        for Npart in N_participants:
-            ID_lst = np.arange(0, Npart)
-            for dataset_size in dataset_sizes:
-                for N_OOI in N_OOIs:
-                    small_condition = np.logical_and(dataset_size == 100, np.logical_or(N_OOI == 25, N_OOI == 50))
+        for dataset_size in dataset_sizes:
+            for Npart in N_participants[1:]:
+                ID_lst = np.arange(0, Npart)
+                N_OOI = 10
 
-                    if not small_condition:
-                        parent_dir = project_path + '/data/Python/transition_matrix/'
-                        directory = "df_part-{}_size-{}_nOOIS-{}".format(Npart, dataset_size, N_OOI)
-                        save_path = os.path.join(parent_dir, directory)
-                        try:
-                            os.mkdir(save_path)
-                        except:
-                            print('Folder already exists')
+                parent_dir = project_path + '\\data\\Python\\transition_matrix\\'
+                directory = "df_part-{}_size-{}_nOOIS-{}".format(Npart, dataset_size, N_OOI)
+                save_path = os.path.join(parent_dir, directory)
 
-                        dataframes, dataloader_deltatime, memory = load_dataframes(project_path, Npart, dataset_size,
-                                                                                   N_OOI)
-                        dataloader_file.write(
-                            "{},{},{},{},{},{} \n".format(Npart, dataset_size, N_OOI, dataloader_deltatime,
-                                                          memory[0], memory[1]))
+                try:
+                    os.mkdir(save_path)
+                except:
+                    print('Folder already exists')
 
-                        matrices, matrices_deltatime, matrices_memory = fl2_transition.create_transition_datasets(
-                            save_path,
-                            ID_lst,
-                            dataframes,
-                            save=True)
-                        matrices_file.write(
-                            "{},{},{},{},{},{} \n".format(Npart, dataset_size, N_OOI, matrices_deltatime,
-                                                          matrices_memory[0], matrices_memory[1]))
-                        del dataframes
-                        # del matrices
-                        gc.collect()
+                dataframes, dataloader_deltatime, memory = load_dataframes(project_path, Npart, dataset_size,
+                                                                           N_OOI)
+                dataloader_file = open(project_path + "/results/Python/dataloader.txt", "a")
+                dataloader_file.write(
+                    "{},{},{},{},{},{} \n".format(Npart, dataset_size, N_OOI, dataloader_deltatime,
+                                                  memory[0], memory[1]))
+                dataloader_file.close()
 
-        dataloader_file.close()
-        matrices_file.close()
+                matrices, matrices_deltatime, matrices_memory = fl2_transition.create_transition_datasets(
+                    save_path,
+                    ID_lst,
+                    dataframes,
+                    save=True)
+                matrices_file = open(project_path + "/results/Python/matrices.txt", "a")
+                matrices_file.write(
+                    "{},{},{},{},{},{} \n".format(Npart, dataset_size, N_OOI, matrices_deltatime,
+                                                  matrices_memory[0], matrices_memory[1]))
+                matrices_file.close()
+                del dataframes
+                # del matrices
+                gc.collect()
+
+
+
 
 
     print(' ')
